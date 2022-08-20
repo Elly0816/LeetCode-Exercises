bbb8873bd4cc7ef432332ccebd6390bdf77275bb
Eleazar Udo <70178556+Elly0816@users.noreply.github.com>
2022-08-20 20:18:11 +0300

    Time: 101 ms (23.53%), Space: 14 MB (85.59%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -2,20 +2,10 @@ class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         # items that appear in both lists
         intersect = []
-        i = 0
-        j = 0
-        while i < len(nums1):
-            while j < len(nums2):
-                if i >= len(nums1) or j >= len(nums2):
-                    return intersect
-                if nums1[i] == nums2[j]:
-                    intersect.append(nums1[i])
-                    nums1.pop(i)
-                    nums2.pop(j)
-                    j = 0
-                    continue
-                j += 1
-            i += 1
-            j = 0
+        # Using a counter to store the data as a dictionary with key = item and value = number of times it appears
+        c = Counter(nums1)
+        for n in nums2:
+            if c[n] > 0:
+                c[n] -= 1
+                intersect.append(n)
         return intersect
-                    
