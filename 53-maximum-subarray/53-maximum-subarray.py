5d17e87585ed83a4c9599d3cff1df9305a4ebbac
Eleazar Udo <70178556+Elly0816@users.noreply.github.com>
2022-08-22 11:44:43 +0300

    Time: 998 ms (65.75%), Space: 27.9 MB (78.72%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,15 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #Save the first value of the array as the max sum
-        current_max = nums[0]
-        global_max = current_max
-        #Check for the larger sub array ending in the nth index
+        current_max = global_max = nums[0]
         for i in range(1, len(nums)):
-            current_max = max(nums[i], current_max+nums[i])
-            # check if the current max is greater than the global max
+            current_max = max(nums[i], current_max + nums[i])
             if current_max > global_max:
                 global_max = current_max
         return global_max
-            
-        
-        
