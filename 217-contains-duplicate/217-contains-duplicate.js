7344ebcb4e47df87f805b15529523e4b45355b11
Eleazar Udo <70178556+Elly0816@users.noreply.github.com>
2022-08-22 11:51:48 +0300

    Time: 121 ms (67.67%), Space: 50.2 MB (85.24%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,12 +3,9 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for (let i=0; i<(nums.length -1); i++){
-     for (let j=i+1; j<nums.length; j++){
-        if (nums[i] === nums[j]){
-            return true;
-        }         
-     }   
+    const numsSet = new Set(nums);
+    if (numsSet.size === nums.length){
+        return false
     }
-    return false;
+    return true
 };
