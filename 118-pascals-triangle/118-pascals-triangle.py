c34a92a7fd8cf997b276a2b933091d91199ed6a6
Eleazar Udo <70178556+Elly0816@users.noreply.github.com>
2022-08-26 01:36:20 +0300

    Time: 34 ms (88.46%), Space: 14 MB (17.88%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -4,12 +4,12 @@ class Solution:
         for i in range(numRows):
             triangle.append([])
         for i in range(len(triangle)):
-             if i == 0:
-                    triangle[i].append(1)
-             else:
+            if i == 0:
+                triangle[i].append(1)
+            else:
                 for j in range(i+1):
-                        if j-1 < 0 or j >= len(triangle[i-1]):
-                            triangle[i].append(1)
-                        else:
-                            triangle[i].append(triangle[i-1][j-1] + triangle[i-1][j])
+                    if j-1 < 0 or j >= len(triangle[i-1]):
+                        triangle[i].append(1)
+                    else:
+                        triangle[i].append(triangle[i-1][j-1] + triangle[i-1][j])
         return triangle
